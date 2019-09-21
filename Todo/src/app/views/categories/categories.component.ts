397ba63ad42d6adffbbffef6d7ecf169f7b45f6d
Maxim Kurganskiy <it.maxk@gmail.com>
2019-09-21 21:05:50 +0300

    Добавлены объекты Subject и подписчики для отображения задач при выборе категории (все задачи пока что не показываются)

--- Todo/src/app/views/categories/categories.component.ts
+++ Todo/src/app/views/categories/categories.component.ts
@@ -20,6 +20,6 @@ export class CategoriesComponent implements OnInit {
 
 
     showTasksByCategory(category: Category) {
-        this.dataHandler.getTasksByCategory(category);
+        this.dataHandler.fillTasksByCategory(category);
     }
 }
