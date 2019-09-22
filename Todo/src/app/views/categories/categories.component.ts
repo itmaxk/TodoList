9ee11fbe243b0661cb8c86a5c775f01fd38d06c9
Maxim Kurganskiy <it.maxk@gmail.com>
2019-09-22 17:11:29 +0300

    Добавлен чекбокс к задачам

--- Todo/src/app/views/categories/categories.component.ts
+++ Todo/src/app/views/categories/categories.component.ts
@@ -10,6 +10,7 @@ import {Category} from "../../model/Category";
 export class CategoriesComponent implements OnInit {
 
     categories: Category[];
+    selectedCategory: Category;
 
     constructor(private dataHandler: DataHandlerService) {
     }
@@ -24,6 +25,8 @@ export class CategoriesComponent implements OnInit {
 
 
     showTasksByCategory(category: Category) {
+        this.selectedCategory = category;
         this.dataHandler.fillTasksByCategory(category);
+
     }
 }
